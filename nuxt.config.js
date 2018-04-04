f4327a613034b2e4160867a136597c85f4226d47
bricksroo <masghahn@gmail.com>
2018-04-03 19:48:00 -0500

    update global site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Mason Hahn',
+    title: 'bricksroo || Mason Hahn',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
