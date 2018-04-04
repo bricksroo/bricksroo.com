05da8159674d699c1a5b88651d4a204a82908c70
bricksroo <masghahn@gmail.com>
2018-04-03 19:47:11 -0500

    footer component changes

---
+++ components/TheFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="f6">
+    <span>&copy; {{thisYear}} Mason Hahn</span> 
+    <span class="mh2">|</span>
+    <span>Created with <a href="https://nuxtjs.org" target="_blank">Nuxt</a> and hosted on <a href="">Netlify</a></span>
+    <span class="mh2">|</span>
+    <span><a href="">Source</a></span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+  computed: {
+    thisYear() {
+      const today = new Date()
+      return today.getFullYear()
+    }
+  }
+}
+</script>
+
+<style>
+footer {
+  grid-column: 1 / -1;
+}
+</style>
