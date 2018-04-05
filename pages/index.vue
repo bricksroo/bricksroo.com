f1c88b72dffdb7a6bf0506c07f704723d3854147
bricksroo <masghahn@gmail.com>
2018-04-04 23:20:33 -0700

    add wip message

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="mt5 measure">
-    <h4 class="mt0 mb2 fw4 f4 lh-copy">Hi, I'm Mason.</h4>
+    <h4 class="mt0 mb2 fw4 f4 lh-copy">Hi, I'm Mason and this a WIP of my personal site.</h4>
     <p class="mt0 mb2 lh-copy">I'm a structural engineer turned front-end developer who loves design, subtleties, and sports.</p>
     <p class="mt0 mb2 lh-copy">This is my personal site. It's the start of something...</p>
   </section>
