ca3c4190d8dab65b99dca39cb627adf7d0e384f8
bricksroo <mason@bricksroo.com>
2019-06-11 22:20:38 -0700

    remove un-edited tailwind config + update to tailwind 1.0 naming convention

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,40 +13,40 @@
         ></g-image>
         <div>
           <h1
-            class="mb-1 sm:mb-0 leading-none text-2xl sm:text-3xl font-extrabold uppercase text-grey-darkest"
+            class="mb-1 sm:mb-0 leading-none text-2xl sm:text-3xl font-extrabold uppercase text-gray-800"
           >
             Mason Hahn
           </h1>
           <h3
-            class="sm:mr-4 mb-3 w-full sm:w-auto text-base sm:text-lg font-normal normal-case text-grey-darker"
+            class="sm:mr-4 mb-3 w-full sm:w-auto text-base sm:text-lg font-normal normal-case text-gray-700"
           >
             @bricksroo
           </h3>
           <div class="flex flex-wrap text-base sm:text-lg">
             <a
               href="https://twitter.com/bricksroo"
-              class="mr-4 font-semibold text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+              class="mr-4 font-semibold text-indigo-700 hover:text-purple-600 underline hover:no-underline"
               title="@bricksroo on Twitter"
             >
               Twitter
             </a>
             <a
               href="https://github.com/bricksroo"
-              class="mr-4 font-semibold text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+              class="mr-4 font-semibold text-indigo-700 hover:text-purple-600 underline hover:no-underline"
               title="@bricksroo on GitHub"
             >
               GitHub
             </a>
             <a
               href="mailto:mason@bricksroo.com"
-              class="mr-4 font-semibold text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+              class="mr-4 font-semibold text-indigo-700 hover:text-purple-600 underline hover:no-underline"
               title="Send me an email at mason@bricksroo.com"
             >
               Email
             </a>
             <a
               href="/mason-hahn-resume-2019-03-31.pdf"
-              class="font-semibold text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+              class="font-semibold text-indigo-700 hover:text-purple-600 underline hover:no-underline"
               title="Check out my resume"
             >
               Resume
@@ -55,28 +55,28 @@
         </div>
       </div>
 
-      <p class="mt-0 mb-4 leading-normal text-black text-base sm:text-lg">
+      <p class="mt-0 mb-4 leading-normal text-gray-900 text-base sm:text-lg">
         Hi, my name is Mason. I'm a front end developer from Costa Mesa, CA who
         currently works at LA Fitness on internal projects. I love design,
         subtleties, and asking "Why?". My main tools these days (besides
         straight HTML, JavaScript, and CSS) are
         <a
           href="https://vuejs.org/"
-          class="text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+          class="text-indigo-700 hover:text-purple-600 underline hover:no-underline"
           >Vue.js</a
         >
         and
         <a
           href="https://tailwindcss.com/docs/what-is-tailwind/"
-          class="text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+          class="text-indigo-700 hover:text-purple-600 underline hover:no-underline"
           >Tailwind CSS</a
         >, but I love learning and exploring new technology and strategies.
       </p>
-      <p class="mt-0 mb-8 leading-normal text-black text-base sm:text-lg">
+      <p class="mt-0 mb-8 leading-normal text-gray-900 text-base sm:text-lg">
         I have a degree in Architectural Engineering from
         <a
           href="https://www.calpoly.edu/"
-          class="text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+          class="text-indigo-700 hover:text-purple-600 underline hover:no-underline"
           >Cal Poly SLO</a
         >
         and worked as a Structural Designer, for a bit, after school until
@@ -84,37 +84,37 @@
         projects, but also some wild stuff like
         <a
           href="https://vitamotus.com/project/absolut-nights-zedd-brooklyn-2014/"
-          class="text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+          class="text-indigo-700 hover:text-purple-600 underline hover:no-underline"
         >
           this giant disco ball
         </a>
         and
         <a
           href="http://thedolab.com/structure/big-fish/"
-          class="text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+          class="text-indigo-700 hover:text-purple-600 underline hover:no-underline"
         >
           this Big Fish stage</a
         >. I love exploring new places, barbell training, and any kind of game,
         whether it's sports, video games, or board&nbsp;games.
       </p>
-      <p class="text-xs text-grey-darker"
+      <p class="text-xs text-gray-700"
         >Built with
         <a
           href="https://gridsome.org/"
-          class="text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+          class="text-indigo-700 hover:text-purple-600 underline hover:no-underline"
           title="Gridsome website"
           >Gridsome</a
         >
         and hosted on
         <a
           href="https://www.netlify.com/"
-          class="text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+          class="text-indigo-700 hover:text-purple-600 underline hover:no-underline"
           title="Netlify website"
           >Netlify</a
         >.
         <a
           href="https://github.com/bricksroo/bricksroo.com"
-          class="text-indigo-dark hover:text-purple-dark underline hover:no-underline"
+          class="text-indigo-700 hover:text-purple-600 underline hover:no-underline"
           title="Source code for this website"
           >Source</a
         ></p
